7bf11b3a6efe6b7aa6ad25926be0dce4d43eebbd
thrylokya10 <45120575+thrylokya10@users.noreply.github.com>
2020-07-31 22:40:56 +0530

    commit_again

--- notebooks/Users/tbikkavo@amgen.com/jenkins.py
+++ notebooks/Users/tbikkavo@amgen.com/jenkins.py
@@ -1,3 +1,3 @@
 # Databricks notebook source
-cohort_list_new_thrylokya=spark.table("dev_mai_upgrade.cohort_list_kyprolis")
-display(cohort_list_new_thrylokya)
+cohort_list_new_thrylokya_1=spark.table("dev_mai_upgrade.cohort_list_kyprolis")
+display(cohort_list_new_thrylokya_1)
