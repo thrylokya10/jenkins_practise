012c6c38df0e340bb7513ed127b059cb141fefa4
thrylokya10 <45120575+thrylokya10@users.noreply.github.com>
2020-07-31 22:09:27 +0530

    commit second_time

--- notebooks/Users/tbikkavo@amgen.com/jenkins.py
+++ notebooks/Users/tbikkavo@amgen.com/jenkins.py
@@ -1,3 +1,3 @@
 # Databricks notebook source
-cohort_list_1=spark.table("dev_mai_upgrade.cohort_list_kyprolis")
-display(cohort_list_1)
+cohort_list_new=spark.table("dev_mai_upgrade.cohort_list_kyprolis")
+display(cohort_list_new)
