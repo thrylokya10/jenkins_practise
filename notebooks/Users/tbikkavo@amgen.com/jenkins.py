da8bf33f97c4d2cf8135b405837bb8236ef8ca06
thrylokya10 <45120575+thrylokya10@users.noreply.github.com>
2020-07-31 22:36:35 +0530

    Update jenkins.py

--- notebooks/Users/tbikkavo@amgen.com/jenkins.py
+++ notebooks/Users/tbikkavo@amgen.com/jenkins.py
@@ -1,3 +1,3 @@
 # Databricks notebook source
-cohort_list_new=spark.table("dev_mai_upgrade.cohort_list_kyprolis")
-display(cohort_list_new)
+cohort_list_new_practise=spark.table("dev_mai_upgrade.cohort_list_kyprolis")
+display(cohort_list_new_practise)
