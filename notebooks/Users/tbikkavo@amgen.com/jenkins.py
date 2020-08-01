57b549128c973709f18e5dc282d846e3b0a1e57e
thrylokya10 <45120575+thrylokya10@users.noreply.github.com>
2020-08-01 07:17:17 +0530

    first connect

--- notebooks/Users/tbikkavo@amgen.com/jenkins.py
+++ notebooks/Users/tbikkavo@amgen.com/jenkins.py
@@ -1,3 +1,3 @@
 # Databricks notebook source
-cohort_list_new_thrylokya_1=spark.table("dev_mai_upgrade.cohort_list_kyprolis")
-display(cohort_list_new_thrylokya_1)
+sat=spark.table("dev_mai_upgrade.cohort_list_kyprolis")
+display(sat)
