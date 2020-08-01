a05fc96609de91747c3252c643b763a3e4e162ff
thrylokya10 <45120575+thrylokya10@users.noreply.github.com>
2020-08-01 07:29:33 +0530

    fourth

--- notebooks/Users/tbikkavo@amgen.com/jenkins.py
+++ notebooks/Users/tbikkavo@amgen.com/jenkins.py
@@ -1,3 +1,3 @@
 # Databricks notebook source
-sat_thrird=spark.table("dev_mai_upgrade.cohort_list_kyprolis")
-display(sat_thrird)
+sat_4=spark.table("dev_mai_upgrade.cohort_list_kyprolis")
+display(sat_4)
