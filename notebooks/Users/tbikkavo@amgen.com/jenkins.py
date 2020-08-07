8a380bba71e0e8721cb7c59a4e06e8919ab28459
thrylokya10 <45120575+thrylokya10@users.noreply.github.com>
2020-08-07 10:44:13 +0530

    friday_firstcommit

--- notebooks/Users/tbikkavo@amgen.com/jenkins.py
+++ notebooks/Users/tbikkavo@amgen.com/jenkins.py
@@ -1,3 +1,3 @@
 # Databricks notebook source
-sunday_5=spark.table("dev_mai_upgrade.cohort_list_kyprolis")
-display(sunday_5)
+friday_1=spark.table("dev_mai_upgrade.cohort_list_kyprolis")
+display(friday_1)
